6b73ebb692c33d2dd22330a5ea28aa570c54343c
jared nielsen <nielsen.jared@gmail.com>
2018-04-21 08:39:17 -0400

    sound

---
+++ README.md
@@ -0,0 +1,8 @@
+# ReactVR + Sound
+
+Simple VR app demonstrating model import and animation, sound and button clicks. 
+
+## Sources and Resources
+* [Samples](https://www.looperman.com/users/loops/219480)
+* [ReactVR Sound](https://facebook.github.io/react-vr/docs/sound.html)
+* [ReactVR VrButton](https://facebook.github.io/react-vr/docs/vrbutton.html)
--- index.vr.js
+++ index.vr.js
@@ -6,17 +6,55 @@ import {
   Text,
   View,
   Model,
-  Sound
+  Animated,
+  AmbientLight,
+  PointLight,
+  Sound,
+  VrButton
 } from 'react-vr';
 
+import {
+  Easing
+} from 'react-native';
+
+const AnimatedModel = Animated.createAnimatedComponent(Model);
+
 export default class ReactVR_MicDrop extends React.Component {
+  state = {
+    rotation: new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    this.rotate();
+  }
+
+  rotate = () => {
+    this.state.rotation.setValue(0);
+    Animated.timing(
+      this.state.rotation,
+      {
+        toValue: 360,
+        duration: 10000,
+        easing: Easing.linear,
+      }
+    ).start(this.rotate);
+  }
+
   render() {
     return (
       <View>
         <Pano source={asset('bg.jpg')}/>
-        {/* <Text
+        <AmbientLight intensity={0.5} />
+        <PointLight
           style={{
-            backgroundColor: '#777879',
+            color: 'white',
+            transform: [
+              {translate: [0, 0, 0]}
+            ]
+          }}
+        />
+        <Text
+          style={{
             fontSize: 0.8,
             fontWeight: '400',
             layoutOrigin: [0.5, 0.5],
@@ -24,23 +62,74 @@ export default class ReactVR_MicDrop extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [
+              {translate: [0, 0, 4]},
+              {rotateY: 180},
+            ],
           }}>
-          hello
-        </Text> */}
+          Grab the mic!
+        </Text>
         <Model
+          lit
           source={{
-            obj: asset('untitled.obj'),
-            mtl: asset('untitled.mtl')
+            obj: asset('speaker.obj'),
+            mtl: asset('speaker.mtl')
+          }}
+          style={{
+            color: "#666",
+            transform: [
+              {translate: [4, 0, 0]},
+              {scale: 0.25},
+              {rotateZ: 180},
+              {rotateX: 180}
+            ]
+          }}
+        >
+          <Sound
+            source={{ wav: asset('drums.wav') }}
+            loop={true}
+          />
+        </Model>
+        <Model
+          lit
+          source={{
+            obj: asset('speaker.obj'),
+            mtl: asset('speaker.mtl')
+          }}
+          style={{
+            color: "#666",
+            transform: [
+              {translate: [-4, 0, 0]},
+              {scale: 0.25}
+            ]
+          }}
+        >
+          <Sound
+            source={{ wav: asset('choir.wav') }}
+            loop={true}
+          />
+        </Model>
+        <VrButton
+          onClickSound={{ wav: asset('mic.wav') }}
+          >
+        <AnimatedModel
+          lit
+          source={{
+            obj: asset('mic.obj'),
+            mtl: asset('mic.mtl')
           }}
           style={{
             color: "#666",
             transform: [
               {translate: [0, 0, -4]},
-              {scale: 0.25}
+              {scale: 0.5},
+              {rotateX: this.state.rotation},
+              {rotateY: this.state.rotation},
+              {rotateZ: this.state.rotation}
             ]
           }}
         />
+      </VrButton>
       </View>
     );
   }
